b783e1185194502d731d68ea8bea154f7903ee19
subham <subhamsarangi23@gmail.com>
2025-09-10 20:49:01 +0530

    Add EmailService to send invoices via email - Implement sendInvoiceEmail() to send PDF invoice attachments - Use JavaMailSender and MimeMessageHelper for email creation - Attach invoice as a ByteArrayResource with dynamic filename

---
+++ src/main/java/com/invoicegeneratorapi/service/EmailService.java
@@ -0,0 +1,37 @@
+package com.invoicegeneratorapi.service;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+
+    @Autowired
+    private  JavaMailSender mailSender;
+
+    @Value("${spring.mail.properties.mail.smtp.from}")
+    private String fromMail;
+
+    public void sendInvoiceEmail(String toEmail, MultipartFile file)throws MessagingException, IOException {
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message,true);
+        helper.setFrom(fromMail);
+        helper.setTo(toEmail);
+        helper.setSubject("Your Invoice");
+        helper.setText("Dear Customer, \n\nPlease find attached your invoice.\n\n Thank you ");
+        String fileName = "invoice_"+System.currentTimeMillis()+".pdf";
+        helper.addAttachment(fileName, new ByteArrayResource(file.getBytes()));
+        mailSender.send(message);
+    }
+}
